3ed76d8b3e5a4b8b71909bd5a3bc9dabcdd19d11
Marcus Edel <marcus.edel@fu-berlin.de>
2014-05-03 15:16:24 +0200

    Test the deploy script with the pelican makefile.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,9 +6,7 @@ AUTHOR = u'gsoc'
 SITENAME = u''
 SITEURL = ''
 AVATAR_IMG = ''
-
 TIMEZONE = 'US/Central'
-
 DEFAULT_LANG = u'en'
 DISQUS_SITENAME = 'gsocmlpack'
 
@@ -16,7 +14,6 @@ DISQUS_SITENAME = 'gsocmlpack'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
-
 TAG_FEED_ATOM = "feeds/tag_%s.atom.xml"
 
 # Blogroll
@@ -24,16 +21,7 @@ LINKS =  (('Pelican', 'http://getpelican.com/'),
           ('Python.org', 'http://python.org/'),
           ('Jinja2', 'http://jinja.pocoo.org/'),)
 
-# Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-#           ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 10
-
 PLUGIN_PATH = 'pelican-plugins'
 PLUGINS = ['assets']
-
 THEME = "theme"
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
